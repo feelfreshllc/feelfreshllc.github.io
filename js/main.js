29376358d858b6c87f1264a4d0e39c37de4486e0
Xhoana Koca <xhoanak@users.noreply.github.com>
2022-03-24 20:58:48 -0400

    add contact form

--- js/main.js
+++ js/main.js
@@ -217,54 +217,11 @@
 	/*----------------------------------------------------*/
 	/*	contact form
 	------------------------------------------------------*/
-
-	/* local validation */
-	$('#contactForm').validate({
-
-		/* submit via ajax */
-		submitHandler: function(form) {
-
-			var sLoader = $('#submit-loader');
-
-			$.ajax({      	
-
-		      type: "POST",
-		      url: "inc/sendEmail.php",
-		      data: $(form).serialize(),
-		      beforeSend: function() { 
-
-		      	sLoader.fadeIn(); 
-
-		      },
-		      success: function(msg) {
-
-	            // Message was sent
-	            if (msg == 'OK') {
-	            	sLoader.fadeOut(); 
-	               $('#message-warning').hide();
-	               $('#contactForm').fadeOut();
-	               $('#message-success').fadeIn();   
-	            }
-	            // There was an error
-	            else {
-	            	sLoader.fadeOut(); 
-	               $('#message-warning').html(msg);
-		            $('#message-warning').fadeIn();
-	            }
-
-		      },
-		      error: function() {
-
-		      	sLoader.fadeOut(); 
-		      	$('#message-warning').html("Something went wrong. Please try again.");
-		         $('#message-warning').fadeIn();
-
-		      }
-
-	      });     		
-  		}
-
-	});
+	var submitted=false;
+	$('#contactForm').on('submit', function(e) {
+		$('#contactForm *').fadeOut(2000);
+		$('#contactForm').prepend('Your message was sent, thank you!');
+		});
 	
 
 })(jQuery);
